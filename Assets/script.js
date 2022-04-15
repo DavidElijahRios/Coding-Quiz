3b0753995b89a1b863769c48f5b42bb8b7a7bb89
David Rios <david.riostech@outlook.com>
2022-04-14 21:58:54 -0700

    made HTML Skeleton and also worked up JS to get working on code quiz functionality

---
+++ Assets/script.js
@@ -0,0 +1,80 @@
+
+var quizContainer = document.querySelector('#quiz');
+
+// I need to create an object of questions and the answers an array within the object to easily access data
+var questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: {
+            a: 'Hyper Text Main Language',
+            b: 'Hover Technology Monitor Language',
+            c: 'Hyper Text Markup Language',
+            d: 'Hover Text Machine Language'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "what syntax is used to make a comment in JavaScript?",
+        answers: {
+            a: '<!-- -->',
+            b: '//',
+            c: '/**/',
+            d: 'None of the above'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "What does API Stand for?",
+        answers: {
+            a: 'Apes playing Instruments',
+            b: 'Application Planning Intel',
+            c: 'Adjusting Programming Interface',
+            d: 'Application Programing Interface'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "What does CSS Stand for?",
+        answers: {
+            a: 'Cascading Style Sheet',
+            b: 'Central Styling Sheet',
+            c: 'Cascading Style Syntax',
+            d: 'Color Style Sheet'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "What is used for HTML comment?",
+        answers: {
+            a: '//',
+            b: '/**/',
+            c: '<!-- -->',
+            d: 'None of the above'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "What does JS stand for?",
+        answers: {
+            a: 'Just Saying',
+            b: 'JavaScript',
+            c: 'JustinSelena',
+            d: 'JavaSyntax'
+        },
+        correctAnswer: 'b'
+    },
+
+];
+//need to create a function to start the quiz 
+function startQuiz() {
+    // Ill need an empty array to to store the output of the answer choices
+    
+    
+}
+ startQuiz()
+
+//function will need quiz questions
+//funtion will need a place to put quiz
+//function will need a place for the results
+//function will need a submit button
+
